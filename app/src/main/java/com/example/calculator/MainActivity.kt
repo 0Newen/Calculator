937fb0a7a4599c743b2e56e1e32b6a88ce4c1a8e
0Newen <esteban.floridocorp@gmeil.com>
2021-08-14 19:06:42 -0500

    -last changes and comments...

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -10,6 +10,7 @@ import java.lang.ArithmeticException
 
 class MainActivity : AppCompatActivity() {
 
+    //Variables Flags de validación.
     var lastNumeric:Boolean=false
     var lastDot:Boolean=false
 
@@ -18,17 +19,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
+    //Función para la captura de digitos.
     fun onDigit(view: View) {
         txt_view.append((view as Button).text)
         lastNumeric=true
     }
 
+    //Función para el boton limpiar.
     fun clearBtn(view: View){
         txt_view.text="";
         lastNumeric=false
         lastDot=false
     }
 
+    //Función para el punto.
     fun decimalPoint(view: View){
         if(lastNumeric&&!lastDot){
             txt_view.append(".")
@@ -37,6 +41,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    //Función de asignación de operador.
     fun onOperator(view: View){
         if(lastNumeric && !isOperatorAdded(txt_view.text.toString()))
             txt_view.append((view as Button).text)
@@ -45,6 +50,7 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    //Función de validación de operadores añadidos.
     private fun isOperatorAdded(value:String):Boolean{
         return  if(value.startsWith("-")){
             false
@@ -54,6 +60,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    //Función para la eliminación del .0 resultado de la operación natural (2-3=1.0)
     private fun removeZero(result: String):String{
         var value=result
         if(result.contains(".0")){
@@ -62,57 +69,69 @@ class MainActivity : AppCompatActivity() {
         return value
     }
 
+    //Función de ejecución de las operaciones.
     fun onEqual(view: View){
+        //Valida si la bandera de númerico esta activa.
         if(lastNumeric){
+            //Captura el texto del TextView
             var txtValue=txt_view.text.toString();
             var prefix=""
+
+            //Bloque manejador de errores para la ejecución de las operaciones.
             try{
+                //Bloque de operación Resta
                 if(txtValue.startsWith("-")){
                     prefix="-"
                     txtValue=txtValue.substring(1)
                 }
                 if(txtValue.contains("-")){
-                    var splitValue=txtValue.split("-")
+                    val splitValue=txtValue.split("-")
                     var one=splitValue[0]
-                    var two=splitValue[1]
+                    val two=splitValue[1]
 
                     if(!prefix.isEmpty()){
                         one=prefix+one
                     }
+                    //TextView de manejo de de visualización
                     txt_view.text=removeZero((one.toDouble()-two.toDouble()).toString())
                 }
-
+                //Bloque de operación Suma
                 if(txtValue.contains("+")){
-                    var splitValue=txtValue.split("+")
+                    val splitValue=txtValue.split("+")
                     var one=splitValue[0]
-                    var two=splitValue[1]
+                    val two=splitValue[1]
 
                     if(!prefix.isEmpty()){
                         one=prefix+one
                     }
+                    //TextView de manejo de de visualización
                     txt_view.text=removeZero((one.toDouble()+two.toDouble()).toString())
                 }
 
+                //Bloque de operación Multiplicación
                 if(txtValue.contains("*")){
-                    var splitValue=txtValue.split("*")
+                    val splitValue=txtValue.split("*")
                     var one=splitValue[0]
-                    var two=splitValue[1]
+                    val two=splitValue[1]
 
                     if(!prefix.isEmpty()){
                         one=prefix+one
                     }
+                    //TextView de manejo de de visualización
                     txt_view.text=removeZero((one.toDouble()*two.toDouble()).toString())
                 }
 
+                //Bloque de operación División
                 if(txtValue.contains("/")){
-                    var splitValue=txtValue.split("*")
+                    var splitValue=txtValue.split("/")
                     var one=splitValue[0]
                     var two=splitValue[1]
 
                     if(!prefix.isEmpty()){
                         one=prefix+one
                     }
-                    txt_view.text=removeZero((one.toDouble()/two.toDouble()).toString())
+                    //TextView de manejo de de visualización
+                    txt_view.text=if(two.toDouble()!=0.0) removeZero((one.toDouble()/two.toDouble()).toString()) else "AnsError"
                 }
             }catch (e: ArithmeticException){
                 e.printStackTrace()
